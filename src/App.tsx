ef338d5b5efb2971d5872fad94cd8fc5441c94f8
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-03-28 12:36:58 +0000

    create player component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import PlayerPage from "./features/player";
 
 function App() {
-  return <></>;
+  return (
+    <main className="flex flex-col items-center justify-center w-full p-2">
+      <PlayerPage />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/features/player/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Player, type PlayerRef } from "@remotion/player";
+import { useCallback } from "react";
+import { MyComposition } from "../../remotion/Composition";
+import { useRemotionPlayerStore } from "@/stores/remotion-player/store";
+
+export default function PlayerPage() {
+  const setPlayer = useRemotionPlayerStore((s) => s.setPlayer);
+
+  const playerRef = useCallback(
+    (node: PlayerRef | null) => {
+      setPlayer(node);
+    },
+    [setPlayer],
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center aspect-video w-full">
+      <Player
+        component={MyComposition}
+        durationInFrames={60}
+        fps={30}
+        compositionWidth={1280 / 2}
+        compositionHeight={720 / 2}
+        ref={playerRef}
+      />
+    </div>
+  );
+}
